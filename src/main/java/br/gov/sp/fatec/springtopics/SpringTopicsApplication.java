7a7c567ff2512c2a7df998cf8bb41a4e807cdcd4
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2023-02-27 14:00:49 -0300

    adding main and test file
    - performing the contextualization of both files.

---
+++ src/main/java/br/gov/sp/fatec/springtopics/SpringTopicsApplication.java
@@ -0,0 +1,12 @@
+package br.gov.sp.fatec.springtopics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTopicsApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringTopicsApplication.class, args);
+    }
+}
---
+++ src/test/java/br/gov/sp/fatec/springtopics/SpringTopicsApplicationTest.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springtopics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTopicsApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
