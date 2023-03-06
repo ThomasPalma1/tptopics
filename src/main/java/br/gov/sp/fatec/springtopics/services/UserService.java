f36889b78316ad7c699e2ce4a79a131b33fb77f8
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2023-03-06 09:35:11 -0300

    creating search by user
    - it is possible to search for a specific user by id.

--- src/main/java/br/gov/sp/fatec/springtopics/services/UserService.java
+++ src/main/java/br/gov/sp/fatec/springtopics/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -25,4 +26,13 @@ public class UserService {
     public List<User> fetchAllUsers() {
         return userRepo.findAll();
     }
+
+    public User userFindByID(Long id) {
+        Optional<User> userOp = userRepo.findById(id);
+        if (userOp.isEmpty()) {
+            throw new IllegalArgumentException("User does not exist");
+        }
+        return userOp.get();
+    }
+
 }
