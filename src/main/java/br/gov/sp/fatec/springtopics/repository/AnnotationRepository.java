d8a9f07ff18536827b78af6bde9828d3f0635796
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2023-04-10 12:21:55 -0300

    feat: adding queries
    - inside the code is a small description about the two types of queries.

--- src/main/java/br/gov/sp/fatec/springtopics/repository/AnnotationRepository.java
+++ src/main/java/br/gov/sp/fatec/springtopics/repository/AnnotationRepository.java
@@ -2,6 +2,23 @@ package br.gov.sp.fatec.springtopics.repository;
 
 import br.gov.sp.fatec.springtopics.entity.Annotation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.swing.*;
+import java.util.List;
 
 public interface AnnotationRepository extends JpaRepository<Annotation, Long> {
+//    There are two ways to perform the query.
+//    The first one is directly from Spring
+//    And the second is using the Query method, which is also from Spring.
+
+    // public List<Annotation> findByTextContains(String text);
+
+    @Query("select a from Annotation a where a.text like %?1%")
+    public List<Annotation> findByText(String text);
+
+    // public List<Annotation> findByUsername(String nameUser);
+
+    @Query("select a from Annotation a join a.usuario u where u.name = ?1")
+    public List<Annotation> findByUsername(String nameUser);
 }
