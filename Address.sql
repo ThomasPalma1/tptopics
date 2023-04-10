2940a73934f261a6c2d7fc4fcef9ba2bc35c9278
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2023-04-10 17:50:20 -0300

    feat: creating address ddl file
    - creating the structure of the table addresses with their respective characteristics.

---
+++ Address.sql
@@ -0,0 +1,17 @@
+use annotation;
+
+drop table if exists add_address;
+
+create table add_address (
+   add_id bigint unsigned not null auto_increment,
+   public_place varchar(100) not null,
+   house_number varchar(10) not null,
+   complement varchar(50) not null,
+   neighborhood varchar(50) not null,
+   city varchar(50) not null,
+   state char(2) not null,
+   zip_code char(8) not null,
+   add_usr_id bigint unsigned not null,
+   primary key (add_id),
+   foreign key add_usr_fk(add_usr_id) references usr_user(usr_id)
+);
